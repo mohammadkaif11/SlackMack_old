ef510d03c61ce6923bc1b6891981e314e7521909
mohammadkaif <mohammadkaif0211@gmail.com>
2022-10-04 16:20:52 +0530

    updating the file

--- index.js
+++ index.js
@@ -80,6 +80,6 @@ io.on('connection', socket => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
